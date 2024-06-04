7e7a269b8210d3609a4d549bcf30a8ab6a947663
sopeti <soepti@gmail.com>
2024-06-04 14:04:27 +0900

    first commit

---
+++ src/snippet/Snippet.java
@@ -0,0 +1,26 @@
+package snippet;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Snippet {
+	public static void main(String[] args) {
+		List<Integer> values = Arrays.asList(4,5,3,4);
+		
+		for(int i =0;i<values.size();i++) {
+			System.out.println(values.get(i));
+			
+		}
+		System.out.println("hello");
+		for(int i:values) {
+			System.out.println(i);
+		}
+		System.out.println("hello");
+		
+		values.forEach(i->System.out.println(i));
+		System.out.println("");
+		System.out.println("hello");
+		
+	}
+}
+
